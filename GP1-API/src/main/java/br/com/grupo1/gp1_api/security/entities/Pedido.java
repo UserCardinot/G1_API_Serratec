46f8d650c28807cc020670a3c1c390b34eabfc66
gabrielcruz07 <cruzsouza2002@gmail.com>
2024-10-21 20:14:39 -0300

    Subindo entidade pedido

---
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/entities/Pedido.java
@@ -0,0 +1,79 @@
+package br.com.grupo1.gp1_api.security.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+	@Table(name = "pedido")
+	public class Pedido {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "ped_cd_id")
+	    private Integer id;
+
+	    @Column(name = "ped_dt_data_pedido")
+	    private LocalDate dataPedido;
+
+	    @Column(name = "ped_tx_status")
+	    private String status;
+	    
+	    @OneToOne
+	    @JoinColumn(name = "ped_fk_carrinho")
+	    private Carrinho carrinho;
+	    
+	    @Column(name = "ped_tx_nf")
+	    private Long nf;
+
+		public Pedido() {
+		}
+
+		public Pedido(Integer id, LocalDate dataPedido, String status, Carrinho carrinho, Long nf) {
+			this.id = id;
+			this.dataPedido = dataPedido;
+			this.status = status;
+			this.carrinho = carrinho;
+			this.nf = nf;
+		}
+
+		public LocalDate getDataPedido() {
+			return dataPedido;
+		}
+
+		public void setDataPedido(LocalDate dataPedido) {
+			this.dataPedido = dataPedido;
+		}
+
+		public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public Long getNf() {
+			return nf;
+		}
+
+		public void setNf(Long nf) {
+			this.nf = nf;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public Carrinho getCarrinho() {
+			return carrinho;
+		}
+	     
+}
