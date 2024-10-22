f84227ba428cc2bfc34d9bca92081f935ddc7dbd
Luan Souza <luan.j3611@hotmail.com>
2024-10-22 08:30:17 -0300

    add repository da classe cliente

---
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/repositories/ClienteRepository.java
@@ -0,0 +1,21 @@
+package br.com.grupo1.gp1_api.security.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.grupo1.gp1_api.security.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+	List<Cliente> findAll();
+
+	Optional<Cliente> findByNome(String nome);
+}
+ 
+
+	
+
