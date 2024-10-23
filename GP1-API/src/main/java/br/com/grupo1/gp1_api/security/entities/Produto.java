29c9147a028a2469f816798262cf412c0c347399
IgorAPSantos <igorppandre@hotmail.com>
2024-10-23 16:38:00 -0300

    add relacao produto e categoria

--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/entities/Produto.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/entities/Produto.java
@@ -1,5 +1,7 @@
 package br.com.grupo1.gp1_api.security.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,80 +15,96 @@ import jakarta.persistence.Table;
 @Table(name = "produto")
 public class Produto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "pro_cd_id")
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "pro_cd_id")
+	private Integer id;
 
-    @Column(name = "pro_txt_nome")
-    private String nome;
+	@ManyToOne
+	@JoinColumn(name = "pro_fk_categoria")
+	@JsonManagedReference
+	private Categoria categoria;
 
-    @Column(name = "pro_txt_descricao")
-    private String descricao;
+	@Column(name = "pro_txt_nome")
+	private String nome;
 
-    @Column(name = "pro_int_estoque")
-    private Integer estoque;
+	@Column(name = "pro_txt_descricao")
+	private String descricao;
 
-    @Column(name = "pro_dbl_preco")
-    private Double preco;
+	@Column(name = "pro_int_estoque")
+	private Integer estoque;
 
-    @ManyToOne
+	@Column(name = "pro_dbl_preco")
+	private Double preco;
+
+	@ManyToOne
 	@JoinColumn(name = "pro_fk_funcionario", nullable = false)
+	@JsonManagedReference
 	private Funcionario funcionario;
 
-    public Produto() {
-    }
+	public Produto() {
+	}
 
-    public Produto(String nome, String descricao, Integer estoque, Double preco, Funcionario funcionario) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.estoque = estoque;
-        this.preco = preco;
-        this.funcionario = funcionario;
-    }
+	public Produto(Categoria categoria, String nome, String descricao, Integer estoque, Double preco,
+			Funcionario funcionario) {
+		this.categoria = categoria;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.estoque = estoque;
+		this.preco = preco;
+		this.funcionario = funcionario;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getDescricao() {
-        return descricao;
-    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public Integer getEstoque() {
-        return estoque;
-    }
+	public Integer getEstoque() {
+		return estoque;
+	}
 
-    public Double getPreco() {
-        return preco;
-    }
+	public Double getPreco() {
+		return preco;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    public void setEstoque(Integer estoque) {
-        this.estoque = estoque;
-    }
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
 
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
 
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 
 }
