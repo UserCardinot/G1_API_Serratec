5d38d89effd9ea878176bfb94ba759bafc953b73
IgorAPSantos <igorppandre@hotmail.com>
2024-10-22 21:10:04 -0300

    implementando put e delete simples na entidade funcionario

---
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/dto/FuncionarioRequestDTO.java
@@ -0,0 +1,24 @@
+package br.com.grupo1.gp1_api.security.dto;
+
+public class FuncionarioRequestDTO {
+
+	private String nome;
+	private String telefone;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
