12ba291cccd1842000ac87e9cf29976b1ebca05b
gabrielcruz07 <cruzsouza2002@gmail.com>
2024-10-22 21:35:36 -0300

    Subindo ProdutoService com get,put,delete

--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/ProdutoService.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/ProdutoService.java
@@ -1,28 +1,58 @@
 package br.com.grupo1.gp1_api.security.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.grupo1.gp1_api.security.dto.ProdutoDTO;
 import br.com.grupo1.gp1_api.security.entities.Produto;
 import br.com.grupo1.gp1_api.security.repositories.ProdutoRepository;
-
 @Service
 public class ProdutoService {
 
-    @Autowired
-    private ProdutoRepository produtoRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
-    public List<Produto> findAll() {
-        return produtoRepository.findAll();
-    }
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
 
-    public Produto findByNome(String nome) {
-        return produtoRepository.findByNome(nome).get();
-    }
+	public Produto findByNome(String nome) {
+		return produtoRepository.findByNome(nome).get();
+	}
 
-    public Produto save(Produto produto) {
-        return produtoRepository.save(produto);
-    }
+	public Produto save(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	public ProdutoDTO getProdutoById(Integer id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		ProdutoDTO produtoDto = new ProdutoDTO();
+		produtoDto.setDescricao(produto.get().getDescricao());
+		produtoDto.setEstoque(produto.get().getEstoque());
+		produtoDto.setNome(produto.get().getNome());
+		produtoDto.setPreco(produto.get().getPreco());
+
+		return produtoDto;
+	}
+
+	public void deletarProduto(Integer id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	public Produto atualizarProduto(Integer id, ProdutoDTO produtoDto) {
+		Optional<Produto> produtoUpdate = produtoRepository.findById(id);
+
+		if (produtoUpdate.isPresent()) {
+			Produto produto = produtoUpdate.get();
+			produto.setNome(produtoDto.getNome());
+			produto.setDescricao(produtoDto.getDescricao());
+			produto.setEstoque(produtoDto.getEstoque());
+			return produtoRepository.save(produto);
+		} else {
+			throw new RuntimeException("ID não encontrado");
+		}
+	}
 }
