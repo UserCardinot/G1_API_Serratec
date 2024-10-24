b8d1074df0ad520c8dadee8a4e1151ec76b66a54
gabrielcruz07 <cruzsouza2002@gmail.com>
2024-10-24 19:52:41 -0300

    PedidoService atualizado

--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/PedidoService.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/PedidoService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 
 import br.com.grupo1.gp1_api.security.dto.PedidoRequestDTO;
 import br.com.grupo1.gp1_api.security.entities.Carrinho;
+import br.com.grupo1.gp1_api.security.entities.Cliente;
 import br.com.grupo1.gp1_api.security.entities.Pedido;
+import br.com.grupo1.gp1_api.security.repositories.CarrinhoRepository;
+import br.com.grupo1.gp1_api.security.repositories.ClienteRepository;
 import br.com.grupo1.gp1_api.security.repositories.PedidoRepository;
 
 @Service
@@ -17,11 +20,14 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
     
-    @Autowired
-    private CarrinhoService carrinhoService;
-    
     @Autowired 
     private ClienteService clienteService;
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
+    @Autowired 
+    private CarrinhoRepository carrinhoRepository;
 
     public List<Pedido> findAll() {
         return pedidoRepository.findAll();
@@ -33,10 +39,10 @@ public class PedidoService {
 
     public Pedido criarPedido(PedidoRequestDTO pedidoRequest, Integer idCliente) {
     	
-        Carrinho carrinhoDoCliente = carrinhoService.exibirCarrinhoByIdCliente(idCliente);
-
+        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+        Optional<Carrinho> carrinho = carrinhoRepository.findByCliente(cliente.get());
         Pedido novoPedido = new Pedido();
-        novoPedido.setCarrinho(carrinhoDoCliente);
+        novoPedido.setCarrinho(carrinho.get());
         novoPedido.setStatus(pedidoRequest.getStatus());
 
         return pedidoRepository.save(novoPedido);
