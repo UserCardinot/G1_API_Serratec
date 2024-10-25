1640f860fa14e9ae988ed2c8eca7590ea6eb6b68
IgorAPSantos <igorppandre@hotmail.com>
2024-10-25 01:18:37 -0300

    finalização crud carrinho

--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/controllers/CarrinhoController.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/controllers/CarrinhoController.java
@@ -3,6 +3,7 @@ package br.com.grupo1.gp1_api.security.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.grupo1.gp1_api.security.dto.CarrinhoRequestDTO;
 import br.com.grupo1.gp1_api.security.dto.CarrinhoResponseDTO;
-import br.com.grupo1.gp1_api.security.entities.Carrinho;
 import br.com.grupo1.gp1_api.security.services.CarrinhoService;
 
 @RestController
@@ -24,7 +24,7 @@ public class CarrinhoController {
 
 	@GetMapping("/{idCliente}")
 	public ResponseEntity<?> exibirCarrinhoByIdCliente(@PathVariable int idCliente) {
-		CarrinhoResponseDTO	 carrinho = carrinhoService.exibirCarrinhoByIdCliente(idCliente);
+		CarrinhoResponseDTO carrinho = carrinhoService.exibirCarrinhoByIdCliente(idCliente);
 
 		if (carrinho == null) {
 			return ResponseEntity.badRequest()
@@ -35,14 +35,34 @@ public class CarrinhoController {
 
 	@PostMapping
 	public ResponseEntity<?> cadastrarCarrinho(@RequestBody CarrinhoRequestDTO carrinhoRequest) {
-		System.out.println(carrinhoRequest.getIdCliente());
-		Carrinho novoCarrinho = carrinhoService.cadastrarCarrinho(carrinhoRequest);
+		CarrinhoResponseDTO novoCarrinho = carrinhoService.cadastrarCarrinho(carrinhoRequest);
 
 		if (novoCarrinho == null) {
-			return ResponseEntity.badRequest().body("Erro: Cliente de id " + carrinhoRequest.getIdCliente() + " não encontrado ou produto de id " + carrinhoRequest.getIdProduto()+" não encontrado");
+			return ResponseEntity.badRequest().body("Erro: Cliente de id " + carrinhoRequest.getIdCliente()
+					+ " não encontrado ou produto de id " + carrinhoRequest.getIdProduto() + " não encontrado");
 		}
 		return ResponseEntity.status(HttpStatus.CREATED).body(novoCarrinho);
 	}
-	
-//	@DeleteMapping
+
+	@DeleteMapping
+	public ResponseEntity<?> removerProdutoCarrinho(@RequestBody CarrinhoRequestDTO carrinhoRequest) {
+		CarrinhoResponseDTO novoCarrinho = carrinhoService.removerProdutoCarrinho(carrinhoRequest);
+
+		if (novoCarrinho == null) {
+			return ResponseEntity.badRequest().body("Erro: Cliente de id " + carrinhoRequest.getIdCliente()
+					+ " não encontrado ou produto de id " + carrinhoRequest.getIdProduto() + " não encontrado");
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoCarrinho);
+	}
+
+	@DeleteMapping("/{idCliente}/limpar")
+	public ResponseEntity<?> limparCarrinho(@PathVariable int idCliente) {
+		CarrinhoResponseDTO novoCarrinho = carrinhoService.limparCarrinho(idCliente);
+
+		if (novoCarrinho == null) {
+			return ResponseEntity.badRequest().body("Erro: Cliente de id " + idCliente
+					+ " não encontrado ou o mesmo ainda não possui um carrinho!");
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoCarrinho);
+	}
 }
--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/CarrinhoService.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/services/CarrinhoService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import br.com.grupo1.gp1_api.security.dto.CarrinhoRequestDTO;
 import br.com.grupo1.gp1_api.security.dto.CarrinhoResponseDTO;
-import br.com.grupo1.gp1_api.security.dto.ProdutoDTO;
 import br.com.grupo1.gp1_api.security.entities.Carrinho;
 import br.com.grupo1.gp1_api.security.entities.Cliente;
 import br.com.grupo1.gp1_api.security.entities.Produto;
@@ -41,19 +40,13 @@ public class CarrinhoService {
 		if (!carrinho.isPresent()) {
 			return null;
 		}
-		CarrinhoResponseDTO carrinhoResponse = new CarrinhoResponseDTO();
-		
-		Set<ProdutoDTO> listaProdutosFormatada = carrinho.get().listaProdutosToListaProdutosDTO();
-		
-		carrinhoResponse.setIdCliente(idCliente);
-		carrinhoResponse.setNomeCliente(cliente.get().getNome());
-		carrinhoResponse.setProdutos(listaProdutosFormatada);
-		carrinhoResponse.setValorTotal(carrinho.get().getTotal());
-		
-		return carrinhoResponse;
+
+		return carrinho.get().toCarrinhoResponseDTO();
+
 	}
 
-	public Carrinho cadastrarCarrinho(CarrinhoRequestDTO carrinhoRequest) {
+	public CarrinhoResponseDTO cadastrarCarrinho(CarrinhoRequestDTO carrinhoRequest) {
+
 		Optional<Cliente> cliente = clienteRepository.findById(carrinhoRequest.getIdCliente());
 
 		if (!cliente.isPresent()) {
@@ -61,28 +54,87 @@ public class CarrinhoService {
 		}
 
 		Optional<Produto> produto = produtoRepository.findById(carrinhoRequest.getIdProduto());
-		
+
 		if (!produto.isPresent()) {
 			return null;
 		}
-		
+
 		Optional<Carrinho> carrinho = carrinhoRepository.findByCliente(cliente.get());
 
 		if (carrinho.isPresent()) {
 			Set<Produto> listaProdutos = carrinho.get().getProdutos();
 			listaProdutos.add(produto.get());
 			carrinho.get().setProdutos(listaProdutos);
-			return carrinhoRepository.save(carrinho.get());
+			carrinhoRepository.save(carrinho.get());
+			return carrinho.get().toCarrinhoResponseDTO();
 		}
+
+		Carrinho novoCarrinho = new Carrinho();
 
 		Set<Produto> novaListaProdutos = new HashSet<>();
 		novaListaProdutos.add(produto.get());
-		
-		Carrinho novoCarrinho = new Carrinho();
+
 		novoCarrinho.setCliente(cliente.get());
 		novoCarrinho.setProdutos(novaListaProdutos);
-		
-		return carrinhoRepository.save(novoCarrinho);
 
+		carrinhoRepository.save(novoCarrinho);
+
+		return carrinho.get().toCarrinhoResponseDTO();
+
+	}
+
+	public CarrinhoResponseDTO removerProdutoCarrinho(CarrinhoRequestDTO carrinhoRequest) {
+
+		Optional<Cliente> cliente = clienteRepository.findById(carrinhoRequest.getIdCliente());
+
+		if (!cliente.isPresent()) {
+			return null;
+		}
+
+		Optional<Produto> produto = produtoRepository.findById(carrinhoRequest.getIdProduto());
+
+		if (!produto.isPresent()) {
+			return null;
+		}
+
+		Optional<Carrinho> carrinho = carrinhoRepository.findByCliente(cliente.get());
+
+		if (!carrinho.isPresent()) {
+			return null;
+		}
+
+		Set<Produto> listaProdutos = carrinho.get().getProdutos();
+
+		listaProdutos.remove(produto.get());
+
+		carrinho.get().setProdutos(listaProdutos);
+
+		carrinhoRepository.save(carrinho.get());
+
+		return carrinho.get().toCarrinhoResponseDTO();
+	}
+
+	public CarrinhoResponseDTO limparCarrinho(int idCliente) {
+
+		Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+
+		if (!cliente.isPresent()) {
+			return null;
+		}
+
+		Optional<Carrinho> carrinho = carrinhoRepository.findByCliente(cliente.get());
+
+		if (!carrinho.isPresent()) {
+			return null;
+		}
+
+		Set<Produto> listaProdutos = carrinho.get().getProdutos();
+		listaProdutos.removeAll(listaProdutos);
+
+		carrinho.get().setProdutos(listaProdutos);
+
+		carrinhoRepository.save(carrinho.get());
+
+		return carrinho.get().toCarrinhoResponseDTO();
 	}
 }
