124f091a388c2d9899f7040be395575d30c8ac6d
IgorAPSantos <igorppandre@hotmail.com>
2024-10-25 01:17:25 -0300

    refatoração na entidade Pedido

--- GP1-API/src/main/java/br/com/grupo1/gp1_api/security/entities/Pedido.java
+++ GP1-API/src/main/java/br/com/grupo1/gp1_api/security/entities/Pedido.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import br.com.grupo1.gp1_api.security.dto.CarrinhoResponseDTO;
+import br.com.grupo1.gp1_api.security.dto.PedidoResponseDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,85 +17,100 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-	@Table(name = "pedido")
-	public class Pedido {
+@Table(name = "pedido")
+public class Pedido {
 
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    @Column(name = "ped_cd_id")
-	    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ped_cd_id")
+	private Integer id;
 
-	    @Column(name = "ped_dt_data_pedido")
-	    private LocalDate dataPedido;
+	@Column(name = "ped_dt_data_pedido")
+	private LocalDate dataPedido;
 
-	    @Column(name = "ped_tx_status")
-	    private String status;
-	    
-	    @OneToOne
-	    @JoinColumn(name = "ped_fk_carrinho")
-		@JsonManagedReference
-	    private Carrinho carrinho;
+	@Column(name = "ped_tx_status")
+	private String status;
 
-		@ManyToOne
-		@JoinColumn(name = "ped_fk_cliente", nullable = false)
-		@JsonManagedReference
-		private Cliente cliente;	
-	    
-	    @Column(name = "ped_tx_nf")
-	    private Long nf;
+	@OneToOne
+	@JoinColumn(name = "ped_fk_carrinho")
+	@JsonManagedReference
+	private Carrinho carrinho;
 
-		public Pedido() {
-		}
+	@ManyToOne
+	@JoinColumn(name = "ped_fk_cliente", nullable = false)
+	@JsonManagedReference
+	private Cliente cliente;
 
-		public Pedido(LocalDate dataPedido, String status, Carrinho carrinho, Cliente cliente, Long nf) {
-			this.dataPedido = dataPedido;
-			this.status = status;
-			this.carrinho = carrinho;
-			this.cliente = cliente;
-			this.nf = nf;
-		}
+	@Column(name = "ped_tx_nf")
+	private Long nf;
 
-		public LocalDate getDataPedido() {
-			return dataPedido;
-		}
+	public Pedido() {
+	}
 
-		public void setDataPedido(LocalDate dataPedido) {
-			this.dataPedido = dataPedido;
-		}
+	public Pedido(LocalDate dataPedido, String status, Carrinho carrinho, Cliente cliente, Long nf) {
+		this.dataPedido = dataPedido;
+		this.status = status;
+		this.carrinho = carrinho;
+		this.cliente = cliente;
+		this.nf = nf;
+	}
 
-		public String getStatus() {
-			return status;
-		}
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
 
-		public void setStatus(String status) {
-			this.status = status;
-		}
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
 
-		public Long getNf() {
-			return nf;
-		}
+	public String getStatus() {
+		return status;
+	}
 
-		public void setNf(Long nf) {
-			this.nf = nf;
-		}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-		public Integer getId() {
-			return id;
-		}
+	public Long getNf() {
+		return nf;
+	}
 
-		public Carrinho getCarrinho() {
-			return carrinho;
-		}
+	public void setNf(Long nf) {
+		this.nf = nf;
+	}
 
-		public void setCarrinho(Carrinho carrinho) {
-			this.carrinho = carrinho;
-		}
+	public Integer getId() {
+		return id;
+	}
 
-		public Cliente getCliente() {
-			return cliente;
-		}
+	public Carrinho getCarrinho() {
+		return carrinho;
+	}
 
-		public void setCliente(Cliente cliente) {
-			this.cliente = cliente;
-		}  
+	public void setCarrinho(Carrinho carrinho) {
+		this.carrinho = carrinho;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public PedidoResponseDTO toPedidoResponseDTO() {
+
+		PedidoResponseDTO pedidoResponse = new PedidoResponseDTO();
+
+		CarrinhoResponseDTO carrinhoResponse = this.carrinho.toCarrinhoResponseDTO();
+
+		pedidoResponse.setCarrinho(carrinhoResponse);
+		pedidoResponse.setDataPedido(this.getDataPedido());
+		pedidoResponse.setIdPedido(this.getId());
+		pedidoResponse.setNf(this.getNf());
+		pedidoResponse.setStatus(this.getStatus());
+
+		return pedidoResponse;
+	}
 }
